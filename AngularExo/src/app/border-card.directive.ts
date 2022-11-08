22c6fa192b5346db38cf69ab30029fe330dc87fc
unknown <Mir-del-dev/Angular-exercice>
2022-11-08 15:08:48 +0100

    exo4

---
+++ AngularExo/src/app/border-card.directive.ts
@@ -0,0 +1,29 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[StudenrBorderCard]'
+})
+export class BorderCardDirective {
+
+  constructor(private el: ElementRef) {
+    this.setBorder('#f5f5f5');
+  }
+
+  @Input('StudentBorderCard') borderColor: string;
+
+  @HostListener('mouseenter') onMouseleave(){
+
+    this.setBorder(this.borderColor || '#009688');
+  };
+  @HostListener('mouseleave') onMouseLeave(){
+    this.setBorder('#f5f5f5');
+  }
+  
+    private setBorder(color: string){
+      let border='solid 4px'+color;
+      this.el.nativeElement.style.border = border;
+
+    }
+   
+
+}
